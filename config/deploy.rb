db2e6fb9a11c35b182cf35b260b5909f67e47850
Jeong Minkyo <jeong.minkyo@nbt.com>
2018-07-16 15:42:01 +0900

    capistrano 배포를 위해 설정을 추가 or 변경하다.

---
+++ config/deploy.rb
@@ -0,0 +1,63 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, '94YB'
+set :repo_url, 'https://github.com/jeongminkyo/94YB.git'
+
+set :deploy_to, '/var/www/94YB'
+set :bundle_flags, ""
+set :use_sudo, true
+set :rbenv_ruby, '2.2.4'
+
+set :passenger_restart_with_sudo, true
+set :passenger_restart_with_touch, true
+set :passenger_in_gemfile, true
+set :passenger_restart_options,
+    -> { "#{deploy_to} --ignore-app-not-running --rolling-restart" }
+
+set :linked_dirs, %w{log}
+
+namespace :deploy do
+  desc 'Restart application'
+  after 'deploy:published', 'nginx:restart'
+
+  task :restart do
+    on roles(:web), in: :sequence, wait: 20 do
+    end
+  end
+  after :finishing, 'deploy:cleanup'
+end
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
