e463bbcba4dd26e5153491f121389d0e6e7439f7
minkyojeong <redwonder@naver.com>
2018-11-09 21:27:50 +0900

    오타 수정

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,6 +1,6 @@
 class KakaoController < ApplicationController
 
   def keyboard
-    render json: { type: 'buttons', button: ['입금 계좌 확인', '납부 내역 확인'] }
+    render json: { type: 'buttons', buttons: ['입금 계좌 확인', '납부 내역 확인'] }
   end
 end
