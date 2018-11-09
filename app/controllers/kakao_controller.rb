eaf69dc9b194edcd39c35b6d007c9c1d27ec5e08
minkyojeong <redwonder@naver.com>
2018-11-10 00:29:14 +0900

    message 수정

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,5 +1,7 @@
 class KakaoController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
   USER_NAME_LIST = %w(민교 재혁 재룡 대훈 준수 태현 남욱 현종 용진 민석 채연)
   def keyboard
     render json: { type: 'buttons', buttons: ['입금 계좌 확인', '납부 내역 확인'] }
@@ -10,19 +12,20 @@ class KakaoController < ApplicationController
     message = {}
 
     keyboard = { type: 'buttons', buttons: ['입금 계좌 확인', '납부 내역 확인'] }
-    result_hash[:keyboard] = keyboard if params[:context] != '납부 내역 확인'
+    result_hash[:keyboard] = keyboard if params[:content] != '납부 내역 확인'
 
     if params[:content] == '입금 계좌 확인'
       message[:text] = '356-1100-0267-33 농협'
-    elsif params[:context] == '납부 내역 확인'
+    elsif params[:content] == '납부 내역 확인'
       message[:text] = '이름을 입력하세요'
-    elsif USER_NAME_LIST.include?(params[:context])
-      user = User.where('display_name like ?', '%' + params[:context] + '%')
+    elsif USER_NAME_LIST.include?(params[:content])
+      user = User.where('display_name like ?', '%' + params[:content] + '%').first
       message[:text] = IncomeHistory.user_income_history(user).to_json(:except => :id)
     else
       message[:text] = '잘못된 이름입니다. 다시 시도해주세요'
     end
 
+    result_hash[:message] = message
     render json: result_hash
   end
 end
