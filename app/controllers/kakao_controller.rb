01172c7d194013e10b1bcb62f4a48570792bdc8c
minkyojeong <redwonder@naver.com>
2018-11-13 00:16:15 +0900

    kakao controller 수정

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -2,7 +2,6 @@ class KakaoController < ApplicationController
 
   skip_before_filter :verify_authenticity_token
 
-  USER_NAME_LIST = %w(민교 재혁 재룡 대훈 준수 태현 남욱 현종 용진 민석 채연 준배)
   def keyboard
     render json: { type: 'buttons', buttons: ['입금 계좌 확인', '납부 내역 확인'] }
   end
@@ -13,26 +12,22 @@ class KakaoController < ApplicationController
 
     keyboard = { type: 'buttons', buttons: ['입금 계좌 확인', '납부 내역 확인'] }
     result_hash[:keyboard] = keyboard if params[:content] != '납부 내역 확인'
+    user = User.where('display_name like ?', '%' + params[:content] + '%').first
 
     if params[:content] == '입금 계좌 확인'
       message[:text] = '356-1100-0267-33 농협'
     elsif params[:content] == '납부 내역 확인'
       message[:text] = '이름을 입력하세요'
-    elsif USER_NAME_LIST.include?(params[:content])
-      user = User.where('display_name like ?', '%' + params[:content] + '%').first
-      if user.present?
-        user_histories = user.cashes
-        if user_histories.present?
-          message[:text] = ''
-          user_histories.each do |history|
-            text = history[:description].to_s + ' : ' + history[:money].to_s + '원' + "\n"
-            message[:text] += text
-          end
-        else
-          message[:text] = '납부 내역이 없습니다.'
+    elsif user.present?
+      user_histories = user.cashes
+      if user_histories.present?
+        message[:text] = ''
+        user_histories.each do |history|
+          text = history[:description].to_s + ' : ' + history[:money].to_s + '원' + "\n"
+          message[:text] += text
         end
       else
-        message[:text] = '아직 등록되지 않은 사용자입니다.'
+        message[:text] = '납부 내역이 없습니다.'
       end
     else
       message[:text] = '잘못된 이름입니다. 다시 시도해주세요'
