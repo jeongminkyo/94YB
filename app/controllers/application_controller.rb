47da2e9fa765801430ea9909a0ec782ec53fd55e
minkyojeong <redwonder@naver.com>
2019-03-10 18:42:56 +0900

    YB01 - 화면 처음 페이지 이미지를 변경한다

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :set_picture
+  def set_picture
+    images = Dir.glob('app/assets/images/*.jpg')
+    @picture_num = images[Random.rand(images.size)].split('/').last
+  end
 end
